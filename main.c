def2d72f1bda42ee5f722f2d7572a37f075f4911
makara <ms6025010087@camtech.edu.kh>
2026-06-24 12:46:53 +0700

    add prototype function

--- main.c
+++ main.c
@@ -6,6 +6,16 @@ struct Patient{
     char diagnosis[100];
     char doctor[50];
 };
+// ly heang
+void add_patient(int size,int amount,struct Patient patients[]);
+// ronith
+void delete_patient(int id,int size,struct Patient patients[]);
+// sithika
+void search(int id,int size,struct Patient patients[]);
+// kimnai
+void update_status(int id,int size,struct Patient patients[]);
+
 void main(){
+
     printf("hello world");
 }
