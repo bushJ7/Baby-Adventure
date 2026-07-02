196d6fc969b46a474edd060541467d748505c4fc
makara <ms6025010087@camtech.edu.kh>
2026-07-02 14:44:34 +0700

    add new functions prototype

--- main.c
+++ main.c
@@ -18,6 +18,17 @@ typedef struct{
     Bill bill;
     Doctor dr;
 }Patient;
+void prompt_id(char id[]);
+void add_patient(Patient patient[],int *size);
+void search_patient(Patient patient[],int size);
+void discharge(Patient patient[],int size);
+void load_data(Patient patient[],int *size);
+void record_info(Patient patient[],int size);
+void assign_doctor(Patient patient[],int size,Doctor Dr[],int dr_amount);
+void calculate_bill(Patient patient[],int size);
+void update_status(Patient patient[],int size);
+int find_index(Patient patient[],int size,char target_id[]);
+void display(Patient patient[],int size);
 void main(){
 
 }
