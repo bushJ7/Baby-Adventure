bdeb8b60383ef616319624ff58e77d5808955f85
engkim506-dev <engkim506@email.com>
2026-07-02 14:32:19 +0700

    add_func_calculate_bill

--- main.c
+++ main.c
@@ -16,49 +16,15 @@ void search(int id,int size,struct Patient patients[]);
 void update_status(int id,int size,struct Patient patients[]);
 
 
-// Diagnosis list and prices
-char *diagnoses[10] = {
-    "Flu", "Diabetes", "Hypertension", "Asthma", "COVID-19",
-    "Allergy", "Migraine", "Tuberculosis", "Cancer Screening", "Heart Disease"
-};
-float prices[10] = {50, 200, 150, 120, 300, 80, 100, 250, 400, 500};
-
-// Function to calculate bill
-void calculateBill() {
-    int choice;
-    char more;
-    float totalBill = 0.0;
-
-    printf("\nChoose your Diagnoses:\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%d. %s - $%.2f\n", i + 1, diagnoses[i], prices[i]);
+void calculate_bill(Patient patient[],int size){
+    char target_id[10];
+    prompt_id(target_id);
+    if(find_index(patient,size,target_id)==-1){
+        printf("patient not found\n");
+        return;
     }
-
-    // Let user select diagnoses
-    do {
-        printf("\nEnter diagnosis number to check: ");
-        scanf("%d", &choice);
-
-        if (choice >= 1 && choice <= 10) {
-            totalBill += prices[choice - 1];
-            printf("Added %s ($%.2f)\n", diagnoses[choice - 1], prices[choice - 1]);
-        } else {
-            printf("Invalid choice!\n");
-        }
-
-        printf("Do you want to add another diagnosis? (y/n): ");
-        scanf(" %c", &more);
-
-    } while (more == 'y' || more == 'Y');
-
-    // Final bill
-    printf("\nFinal Bill: $%.2f\n", totalBill);
-}
-
-
-
-
-void main(){
-
-
-}
+    int index = find_index(patient,size,target_id);
+    printf("enter service bill: ");scanf("%f",&patient[index].bill.serviceFee);
+    printf("enter medecine bill: ");scanf("%f",&patient[index].bill.medicineCost);
+    patient[index].bill.totalBill = patient[index].bill.serviceFee + patient[index].bill.medicineCost;
+    printf("successfully calculate the bill\n");
