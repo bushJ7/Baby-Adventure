ce83b7a4488fc5acc860ebedf95b74dd88f6c918
rp6026010132-beep <rp6026010132@camtech.edu.kh>
2026-07-02 15:45:44 +0700

    function discharge patient and function record patient

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 typedef struct {
     char dr_name[40];
     char specialty[40];
@@ -45,3 +46,45 @@ void calculate_bill(Patient patient[],int size){
     patient[index].bill.totalBill = patient[index].bill.serviceFee + patient[index].bill.medicineCost;
     printf("successfully calculate the bill\n");
 }
+void discharge(Patient patient[],int size){
+    
+    char target_id[10];
+    prompt_id(target_id);
+    if(find_index(patient,size,target_id)==-1){
+        printf("patient not exist!\n");
+        return ;
+    }
+    int index = find_index(patient,size,target_id);
+    if(patient[index].bill.totalBill==0){
+        printf("you hasn't calculate patient's bill yet!\n");
+        return;
+    }
+    printf("enter final diagnose: ");
+    fgets(patient[index].diagnose,sizeof(patient[index].diagnose),stdin);
+    patient[index].diagnose[strcspn(patient[index].diagnose,"\n")] = '\0';
+    strcpy(patient[index].status,"Discharged");
+    printf("\n==================================================\n");
+    printf("           PATIENT DISCHARGE SUMMARY REPORT       \n");
+    printf("==================================================\n");
+    printf(" Patient ID         : %s\n", patient[index].patient_id);
+    printf(" Patient Name       : %s\n", patient[index].name);
+    printf(" Age                : %d\n", patient[index].age);
+    printf(" Status             : %s\n", patient[index].status);
+    printf(" Responsible Doctor : %s\n", patient[index].dr.dr_name);
+    printf(" Final Diagnosis    : %s\n", patient[index].diagnose);
+    printf("--------------------------------------------------\n");
+    printf(" FINANCIAL BREAKDOWN:\n");
+    printf("   Medicine Charges : $%.2f\n", patient[index].bill.medicineCost);
+    printf("   Service/Treat Fee: $%.2f\n", patient[index].bill.serviceFee);
+    printf("--------------------------------------------------\n");
+    printf(" TOTAL AMOUNT DUE : $%.2f\n", patient[index].bill.totalBill);
+    printf("==================================================\n");
+    printf("Patient file finalized successfully.\n");
+}
+void record_info(Patient patient[], int size){
+    FILE *ptr = fopen("patient_information.txt","w");
+
+    if(ptr == NULL){
+        printf("Cannot open file!\n");
+        return;
+    }
