f044e7ea67fcab4bc96732840b7d030988fd509c
rp6026010132-beep <rp6026010132@camtech.edu.kh>
2026-07-04 14:35:19 +0700

    function record information

--- main.c
+++ main.c
@@ -162,3 +162,64 @@ void add_patient(Patient patient[], int *size){
     printf("patient info added successfully!\n");
 }
 
+void discharge(Patient patient[],int size){
+    
+    char target_id[10];
+    prompt_id(target_id);
+    if(find_index(patient,size,target_id)==-1){
+        printf("patient not exist!\n");
+        return ;
+    }
+    int index = find_index(patient,size,target_id);
+    if(patient[index].bill.totalBill==0){
+        printf("you hasn't calculate patient's bill yet!\n");
+        return;
+    }
+    printf("enter final diagnose: ");
+    fgets(patient[index].diagnose,sizeof(patient[index].diagnose),stdin);
+    patient[index].diagnose[strcspn(patient[index].diagnose,"\n")] = '\0';
+    strcpy(patient[index].status,"Discharged");
+    printf("\n==================================================\n");
+    printf("           PATIENT DISCHARGE SUMMARY REPORT       \n");
+    printf("==================================================\n");
+    printf(" Patient ID         : %s\n", patient[index].patient_id);
+    printf(" Patient Name       : %s\n", patient[index].name);
+    printf(" Age                : %d\n", patient[index].age);
+    printf(" Status             : %s\n", patient[index].status);
+    printf(" Responsible Doctor : %s\n", patient[index].dr.dr_name);
+    printf(" Final Diagnosis    : %s\n", patient[index].diagnose);
+    printf("--------------------------------------------------\n");
+    printf(" FINANCIAL BREAKDOWN:\n");
+    printf("   Medicine Charges : $%.2f\n", patient[index].bill.medicineCost);
+    printf("   Service/Treat Fee: $%.2f\n", patient[index].bill.serviceFee);
+    printf("--------------------------------------------------\n");
+    printf(" TOTAL AMOUNT DUE : $%.2f\n", patient[index].bill.totalBill);
+    printf("==================================================\n");
+    printf("Patient file finalized successfully.\n");
+}
+
+void record_info(Patient patient[], int size){
+    FILE *ptr = fopen("patient_information.txt","w");
+    if(ptr == NULL){
+        printf("Cannot open file!\n");
+        return;
+    }
+    // Header
+    fprintf(ptr,
+    "ID|Name|Age|Symptom|Status|Diagnosis|Doctor|Service Fee|Medicine|Total Bill\n");
+    for(int i = 0; i < size; i++){
+        fprintf(ptr,
+        "%s|%s|%d|%s|%s|%s|%s|%.2f|%.2f|%.2f\n",
+        patient[i].patient_id,
+        patient[i].name,
+        patient[i].age,
+        patient[i].symptom,
+        patient[i].status,
+        patient[i].diagnose,
+        patient[i].dr.dr_name,
+        patient[i].bill.serviceFee,
+        patient[i].bill.medicineCost,
+        patient[i].bill.totalBill);
+    }
+    fclose(ptr);
+}
